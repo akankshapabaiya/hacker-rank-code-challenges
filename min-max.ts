9d74fa9c59415c511b70a7fcf42975be35a6fd8a
akankshapabaiya <37528980+akankshapabaiya@users.noreply.github.com>
2025-03-15 02:14:14 +0530

    Create min-max.ts

---
+++ min-max.ts
@@ -0,0 +1,75 @@
+/*Problem
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. 
+She tabulates the number of times she breaks her season record for most points and least points in a game. 
+Points scored in the first game establish her record for the season, and she begins counting from there.
+Scores are in the same order as the games played. She tabulates her results as follows:
+Example: 
+score = [12, 24, 10, 24]
+
+                                     Count
+    Game  Score  Minimum  Maximum   Min Max
+     0      12     12       12       0   0
+     1      24     12       24       0   1
+     2      10     10       24       1   1
+     3      24     10       24       1   1
+Given the scores for a season, determine the number of times Maria breaks her records for most and least points scored during the season.
+*/
+
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+function breakingRecords(scores: number[]): number[] {
+    // Write your code here
+    let minimum : number = scores[0]
+    let maximum : number = scores[0]
+    let minCount : number = 0
+    let maxCount : number = 0
+        for(let score of scores){
+        if(score < minimum){minCount++; minimum = score;}
+        if(score > maximum){maxCount++; maximum = score;}
+    }
+    return [maxCount, minCount];
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const scores: number[] = readLine().replace(/\s+$/g, '').split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result: number[] = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
